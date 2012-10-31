64c6cc199cbd968eef17da2cec81ed141824f58b
Anthony Gelibert <anthony.gelibert@me.com>
2012-10-31 14:54:16 +0100

    Remove tabs and add includes.

--- teste.c
+++ teste.c
@@ -1,45 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
 #include "buddy.h"
 
 int main(void)
 {
-	/**
-	 * Create variable "buddy".
-	 */
-	buddy_t* buddy;
+    buddy_t* buddy;
 
-	/**
-	 * Initialize "buddy" variable.
-	 */
-	buddy = buddy_init();
+    buddy = buddy_init();
 
-	/**
-	 * Activate all i-Buddy gadgets avalaible.
-	 */
-	buddy_activate_all(buddy);
+    buddy_activate_all(buddy);
 
-	/**
-	 * Changes the states of all i-Buddy gadgets every second.
-	 */
-	printf("Blue\n");
-	buddy_state(buddy, BUDDY_OFF, BUDDY_BLUE, BUDDY_DOWN, BUDDY_OFF);
-	sleep(5);
-	printf("Green\n");
-	buddy_state(buddy, BUDDY_ON, BUDDY_GREEN, BUDDY_OFF, BUDDY_RIGHT);
-	sleep(5);
-	printf("Red\n");
-	buddy_state(buddy, BUDDY_OFF, BUDDY_RED, BUDDY_OFF, BUDDY_LEFT);
-	sleep(5);
-	printf("Yellow\n");
-	buddy_state(buddy, BUDDY_ON, BUDDY_YELLOW, BUDDY_UP, BUDDY_RIGHT);
-	sleep(5);
+    printf("Blue\n");
+    buddy_state(buddy, BUDDY_OFF, BUDDY_BLUE, BUDDY_DOWN, BUDDY_OFF);
+    sleep(5);
+    printf("Green\n");
+    buddy_state(buddy, BUDDY_ON, BUDDY_GREEN, BUDDY_OFF, BUDDY_RIGHT);
+    sleep(5);
+    printf("Red\n");
+    buddy_state(buddy, BUDDY_OFF, BUDDY_RED, BUDDY_OFF, BUDDY_LEFT);
+    sleep(5);
+    printf("Yellow\n");
+    buddy_state(buddy, BUDDY_ON, BUDDY_YELLOW, BUDDY_UP, BUDDY_RIGHT);
+    sleep(5);
 
-	/**
-	 * Reset all i-Buddy gadgets.
-	 */
-	buddy_reset(buddy);
+    buddy_reset(buddy);
 
-	/**
-	 * Free memory used.
-	 */
-	buddy_free(buddy);
+    buddy_free(buddy);
 }
